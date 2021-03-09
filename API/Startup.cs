da5d823306dd8f101a7de6e645bf59482b2016b7
Cheskel Sander <cheskel2562@gmail.com>
2021-03-09 00:25:30 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,8 +26,6 @@ namespace API
 
         }
 
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -36,6 +34,7 @@ namespace API
                 Options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +48,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
