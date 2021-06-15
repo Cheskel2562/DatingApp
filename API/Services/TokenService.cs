0a9a06be75ee3835fb18de60298a37f5e620898f
cheskelatevelt <83782293+cheskelatevelt@users.noreply.github.com>
2021-06-15 01:20:33 -0400

    middle of session 42

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames)
+            }
+        }
+    }
+}
