85aced40b6fa636a458d7370dc12ffd373aa39e2
= <=>
2021-06-23 01:19:08 -0400

    middle of session 56

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,25 +10,25 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
+  currentUser$: Observable<User>;
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login()
   {
     this.accountService.login(this.model).subscribe(Response => {
       console.log(Response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
